ba8a34996c17a465c9f8bea5efd94c347d0eca70
PedroB <bereilhp@gmail.com>
2026-01-05 18:52:04 +0100

    bugfix(changing layout): Updating layout for Analytics

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
     <html lang="en" className={GeistSans.className}>
       <body>
         {children}
-        <AxiomWebVitals />
         <Analytics />
+        <AxiomWebVitals />
       </body>
     </html>
   )
